fb884fb74fe4cd49057936ae20bc5d072939ca81
KenCorp <kennethwarui35@gmail.com>
2023-06-19 14:10:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    if(customerName === 'bob'){
+        customerName = customerName.toUpperCase();
+    } else{
+        []
+    }
+
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'Ken'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Abass'
+}
